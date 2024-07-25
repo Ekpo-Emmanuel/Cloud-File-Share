2a8862eeaa6e90df9bd1641ee3cf3ddff16b572f
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-24 22:14:11 -0700

    feat: Dashboard content

---
+++ app/(pages)/dashboard/page.js
@@ -0,0 +1,13 @@
+// dashboard/page.js
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* Dashboard content */}
+    </div>
+  );
+};
+
+export default DashboardPage;
