bfd1cb622f067894cf33c125a3de301784f067bd
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-24 22:06:06 -0700

    feat: Implement AuthContextProvider for user authentication

---
+++ contexts/AuthContext.js
@@ -0,0 +1,31 @@
+'use client'
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from '../libs/firebase/auth';
+
+const AuthContext = createContext({});
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
